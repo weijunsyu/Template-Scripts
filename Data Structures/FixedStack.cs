6cb3b20cc1521cad39d361f18c0dcd88c1b202b0
WeiJun Syu <weijunsyu@gmail.com>
2022-05-23 16:30:37 -0700

    Added MIT license text

--- Data Structures/FixedStack.cs
+++ Data Structures/FixedStack.cs
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2022 WeiJun Syu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 using System;
 
 public class FixedStack<T>
@@ -22,7 +46,6 @@ public class FixedStack<T>
     {
         return _length;
     }
-
     public FixedStack<T> Copy()
     {
         FixedStack<T> copyStack = new FixedStack<T>(_capacity);
@@ -65,11 +88,13 @@ public class FixedStack<T>
             _buffer[i] = default(T);
         }
     }
-    public void Push(T item)
+    // returns a tuple denoting first the status of the result then the purged item if stack was full
+    // where status is given: 1 = stack full, 0 = stack not full, -1 = stack was locked
+    public (int, T) Push(T item)
     {
         if (!_isLocked)
         {
-            // Add to the top of the stack
+            // Add to the top of the stack (stack is empty)
             if (_length == 0)
             {
                 // bot and top currently == -1 thus:
@@ -78,6 +103,7 @@ public class FixedStack<T>
                 _top = 0;
                 _buffer[_top] = item;
             }
+            // Stack is not empty but is not full
             else if (_length < _capacity)
             {
                 _length++;
@@ -87,13 +113,20 @@ public class FixedStack<T>
             // Stack is full (purge last + add new)
             else
             {
-                _buffer[_bottom] = item;
+                T purgedItem = _buffer[_bottom]; // copy item to be purged
+                _buffer[_bottom] = item; // swap out item to be purged
+                // move pointers such that new top is at old bottom (and bottom adjusted accordingly)
                 _top = Modulo(_top + 1, _capacity);
                 _bottom = Modulo(_bottom + 1, _capacity);
+                return (1, purgedItem);
             }
+            return (0, default(T)); // stack was not full
+        }
+        else
+        {
+            return (-1, default(T)); // stack is locked
         }
     }
-
     public T Peek()
     {
         return _buffer[_top];
@@ -112,7 +145,7 @@ public class FixedStack<T>
 
         if (!_isLocked)
         {
-            _top = Math.Abs(Modulo(_top -1, _capacity));
+            _top = Math.Abs(Modulo(_top - 1, _capacity));
 
             if (--_length == 0)
             {
@@ -122,7 +155,6 @@ public class FixedStack<T>
 
         return retValue;
     }
-
     private static int Modulo(int a, int b)
     {
         return (a % b + b) % b;
